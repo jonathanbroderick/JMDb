d533a470a6702abc850b9496607b03b883cb2266
jonathanbroderick <jonathanbroderick007@gmail.com>
2017-04-12 20:32:58 -0700

    Fixed the naming of instance variables and other naming issues
    The added files were dynamically generated by the code.

--- movie_class.py
+++ movie_class.py
@@ -2,5 +2,5 @@ class Movie():
     def __init__(self, title, storyline, img, trailer):
         self.title = title
         self.storyline = storyline
-        self.img = img
-        self.trailer = trailer
+        self.poster_image_url = img
+        self.trailer_youtube_url = trailer
