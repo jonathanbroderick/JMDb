e0eb809bfb2843e5dcfd195f79531fa2e9a2d621
jonathanbroderick <jonathanbroderick007@gmail.com>
2017-04-12 20:05:00 -0700

    Moved movie_class file out of .git directory. Oops!

---
+++ movie_class.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self, title, storyline, img, trailer):
+        self.title = title
+        self.storyline = storyline
+        self.img = img
+        self.trailer = trailer
