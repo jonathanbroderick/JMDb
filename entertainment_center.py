2d0c54f52c2c2cb0f56741251cb61fdcf5e504f5
jonathanbroderick <jonathanbroderick007@gmail.com>
2017-04-16 08:37:50 -0700

    Made code style changes for compliance

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,32 +2,38 @@ import media
 import fresh_tomatoes
 
 # Defining instances of class Movie
-red = media.Movie('RED',
-    'Retired secret agents being hunted down by the very government they served.',
+red = media.Movie(
+    'RED',
+    'Retired secret agents hunted down by the government.',
     'http://www.impawards.com/2010/posters/red_ver7.jpg',
     'https://youtu.be/-JZ_moituIo')
 
-a_team = media.Movie('A-Team',
+a_team = media.Movie(
+    'A-Team',
     'An elite team of soldiers protecting the world.',
     'http://www.impawards.com/2010/posters/a_team_ver12_xlg.jpg',
     'https://youtu.be/kM0ypzvuphg')
 
-iron_man = media.Movie('Iron Man',
-    'A genius, billionaire, playboy, philanthropist turns super hero after enduring a traumatic event.',
+iron_man = media.Movie(
+    'Iron Man',
+    'A billionaire playboy turns super hero.',
     'http://www.freemovieposters.net/posters/iron_man_2_2010_3219_poster.jpg',
     'https://youtu.be/8hYlB38asDY')
 
-sherlock_holmes = media.Movie('Sherlock Holmes',
+sherlock_holmes = media.Movie(
+    'Sherlock Holmes',
     'A brilliant detective thwarts crimes in 19th-century England.',
     'http://www.impawards.com/2009/posters/sherlock_holmes_ver8_xlg.jpg',
     'https://youtu.be/J7nJksXDBWc')
 
-italian_job = media.Movie('The Italian Job',
-    'A group of professional thieves plan a heist to get revenge on an old friend.',
+italian_job = media.Movie(
+    'The Italian Job',
+    'A group of professional thieves plan a heist.',
     'http://www.impawards.com/2003/posters/italian_job.jpg',
     'https://youtu.be/5Eyw-Qiwpj0')
 
-avengers = media.Movie('The Avengers',
+avengers = media.Movie(
+    'The Avengers',
     'An elite group of super heroes takes on alien invaders.',
     'https://images-na.ssl-images-amazon.com/images/I/61bINjWK62L.jpg',
     'https://youtu.be/eOrNdBpGMv8')
